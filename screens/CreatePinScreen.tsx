e35bad7a6ee5965af69cdfff6a74cfad5370c8a2
Matan Shaviro <matan3sh@gmail.com>
2022-04-18 08:44:14 +0300

    Finish CreatePinScreen

--- screens/CreatePinScreen.tsx
+++ screens/CreatePinScreen.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { Button, Image, View, Platform } from "react-native";
+import React, { useState } from "react";
+import { Button, Image, View, TextInput, StyleSheet } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 
 export function CreatePinScreen() {
   const [image, setImage] = useState<string | null>(null);
+  const [title, setTitle] = useState<string>("");
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -21,15 +22,44 @@ export function CreatePinScreen() {
     setImage(image);
   };
 
+  const onSubmit = () => {};
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={styles.root}>
       <Button title="Pick an image from camera roll" onPress={pickImage} />
       {image && (
-        <Image
-          source={{ uri: image }}
-          style={{ width: "100%", aspectRatio: 1 }}
-        />
+        <>
+          <Image source={{ uri: image }} style={styles.image} />
+          <TextInput
+            placeholder="Title"
+            value={title}
+            onChangeText={setTitle}
+            style={styles.input}
+          />
+          <Button title="Create Pin" onPress={onSubmit} />
+        </>
       )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+    marginVertical: 10,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "gainsboro",
+    padding: 5,
+    width: "100%",
+    borderRadius: 5,
+  },
+});
